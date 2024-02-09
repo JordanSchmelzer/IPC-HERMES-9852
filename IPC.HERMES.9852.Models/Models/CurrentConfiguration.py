7aacd51ba749ff2816dec75720496f48c5696115
Jordan Schmelzer <jordanschmelzer@gmail.com>
2024-02-09 09:19:15 -0600

    first draft of Models complete

--- IPC.HERMES.9852.Models/Models/CurrentConfiguration.py
+++ IPC.HERMES.9852.Models/Models/CurrentConfiguration.py
@@ -1,2 +1,15 @@
+'''
+The CurrentConfiguration message is sent by a machine in response to the GetConfiguration message.
+
+For the definition of UpstreamConfiguration and DownstreamConfiguration see section 3.19. 
+If  no  MachineId  has  been  configured  yet,  the  CurrentConfiguration  message  does  not  contain  the  attribute 
+MachineId.
+'''
+
 class CurrentConfiguration:
-    Timestamp=""
+    def __init__(self):
+        self.MachineId=""
+        self.SupervisorySystemPort=65535
+        self.UpstreamConfigurations=[]
+        self.DownstreamConfiguration=[]
+    
--- IPC.HERMES.9852.Models/Models/GetConfiguration.py
+++ IPC.HERMES.9852.Models/Models/GetConfiguration.py
@@ -1,2 +1,14 @@
+'''
+The CurrentConfiguration message is sent by a machine in response to the GetConfiguration message.
+
+For the definition of UpstreamConfiguration and DownstreamConfiguration see section 3.19. 
+If  no  MachineId  has  been  configured  yet,  the  CurrentConfiguration  message  does  not  contain  the  attribute 
+MachineId.
+'''
+
 class GetConfiguration:
-    Timestamp=""
+    def __init__(self):
+        self.MachineId = ""
+        self.SupervisorySystemPort = 65535
+        self.UpstreamConfigurations = []
+        self.DownstreamConfigurations = []
