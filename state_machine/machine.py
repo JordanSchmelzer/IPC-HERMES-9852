41ddaaf78bffce9ca277eedb5283130cdf4556f8
Jordan Schmelzer <jordanschmelzer@gmail.com>
2024-02-28 17:53:38 -0600

    Statemachine

--- state_machine/machine.py
+++ state_machine/machine.py
@@ -1,21 +1,37 @@
-from __future__ import annotations
-from abc import ABC, abstractmethod
-
-class Context:
-    '''
-    The context defines the interface of interest to clients. It also maintains a reference to an instance
-    of a State subclass, which reprresetns the current state of the Context.
-    '''
+'''State class: Base State class'''
+class State:
     
-    _state = None
-    '''
-    A reference to the current state of the Context.
-    '''
-
-    def __init__(self, state: State) -> None:
-        self.transition_to(state)
+    """Base state. This is to share functionality"""
+    def switch(self):
+        """Switch between conveyor belt on and off on DEK machine """
+        pass
+    
+class ConveyorRunningState(State):
+    def __init__(self, conveyor_belt):
+        self.conveyor_belt = conveyor_belt
         
-    def transition_to(self, state: State):
-        '''
-        The Context allows changing the State object at runtime.
-        '''
+    def switch(self):
+        print("Stopping the conveyor belt...")
+        self.conveyor_belt.state = self.conveyor_belt.stopped_state
+            
+class ConveyorStoppedState(State):
+    def __init__(self, conveyor_belt):
+        self.conveyor_belt = conveyor_belt
+        
+    def switch(self):
+        print("Starting the conveyor belt...")
+        self.conveyor_belt.state = self.conveyor_belt.running_state
+        
+class ConveyorBelt:
+    def __init__(self):
+        self.running_state = ConveyorRunningState(self)
+        self.stopped_state = ConveyorStoppedState(self)
+        self.state = self.stopped_state
+        
+    def switch(self):
+        self.state.switch()
+        
+conveyor_belt = ConveyorBelt()
+
+conveyor_belt.switch()
+conveyor_belt.switch()
